4c245e4f62fad051338c592bc542cef602631d5c
Alessandro Iodice <51196101+4l3ss4ndro@users.noreply.github.com>
2022-09-09 11:16:19 +0200

    Add files via upload

---
+++ socket-client.c
@@ -0,0 +1,56 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+typedef struct {
+char var1;
+int var2;
+}mystruct;
+
+int main(int argc, char const* argv[])
+{
+	int sock = 0, valread, client_fd;
+	struct sockaddr_in serv_addr;
+	mystruct* hello;
+	mystruct tosend;
+    hello = &tosend;
+	tosend.var1 = 'A';
+	tosend.var2 = 29;
+	
+	char buffer[1024] = { 0 };
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	// Convert IPv4 and IPv6 addresses from text to binary
+	// form
+	if (inet_pton(AF_INET, "10.0.0.2", &serv_addr.sin_addr)
+		<= 0) {
+		printf(
+			"\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
+	if ((client_fd
+		= connect(sock, (struct sockaddr*)&serv_addr,
+				sizeof(serv_addr)))
+		< 0) {
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	send(sock, hello, sizeof(hello), 0);
+	printf("Struct message sent\n");
+	valread = read(sock, buffer, 1024);
+	printf("%s\n", buffer);
+
+	// closing the connected socket
+	close(client_fd);
+	return 0;
+}
